7de262bce7c9e0de656c98f3d74ee08f9ec54635
atharvakadlag <atharvakadlag@gmail.com>
2022-09-28 14:04:01 +0530

    added parallel_results.txt

--- HPC/hpc-project/simple-perceptron/run.sh
+++ HPC/hpc-project/simple-perceptron/run.sh
@@ -1,3 +1,7 @@
-g++ -fopenmp -o main_parallel main_parallel.cpp
-export OMP_NUM_THREADS=$1
-./main_parallel
+for i in `seq 14`
+do
+	num_threads=$((2**i))
+	echo $num_threads >> main_parallel_results.txt
+	export OMP_NUM_THREADS=$num_threads
+	./main_parallel >> main_parallel_results.txt
+done
